1e34232ba851af8cd531a0132538b04f8a66c5b2
thomaschiozzi-tndigit <thomas.chiozzi@tndigit.it>
2024-07-11 14:58:34 +0200

    fix: cvt_dates options bug

--- internal/jwt/dates.go
+++ internal/jwt/dates.go
@@ -14,31 +14,81 @@ func convertE2UTC(data []byte) ([]byte, error) {
 	if err := json.Unmarshal(data, &v); err != nil {
 		return nil, errors.New("claims are not a json")
 	}
-	m := v.(map[string]interface{})
-	for k, vv := range m {
-		switch vv.(type) {
-		case map[string]interface{}:
-			v1, err := json.Marshal(vv)
-			if err != nil {
-				return nil, fmt.Errorf("internal error: %w", err)
-			}
-			v2, err := convertE2UTC(v1)
+	switch vv := v.(type) {
+	case []interface{}:
+		jArr, err := convertDatesSliceRecursiveStep(vv)
+		if err != nil {
+			return nil, fmt.Errorf("internal error: %w", err)
+		}
+		out, err := json.Marshal(jArr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to re-convert claims to json: %w", err)
+		}
+		return out, nil
+	case map[string]interface{}:
+		jObj, err := convertDatesMapRecursiveStep(vv)
+		if err != nil {
+			return nil, fmt.Errorf("internal error: %w", err)
+		}
+		out, err := json.Marshal(jObj)
+		if err != nil {
+			return nil, fmt.Errorf("failed to re-convert claims to json: %w", err)
+		}
+		return out, nil
+	default:
+		return data, nil
+	}
+}
+
+func convertDatesSliceRecursiveStep(jsonArr []interface{}) ([]interface{}, error) {
+	res := make([]interface{}, len(jsonArr))
+	for i, v := range jsonArr {
+		switch vv := v.(type) {
+		case []interface{}:
+			arr, err := convertDatesSliceRecursiveStep(vv)
 			if err != nil {
 				return nil, err
 			}
-			m[k] = v2
+			res[i] = arr
+		case map[string]interface{}:
+			dic, err := convertDatesMapRecursiveStep(vv)
+			if err != nil {
+				return nil, err
+			}
+			res[i] = dic
+		default:
+			res[i] = vv
+		}
+	}
+	return res, nil
+}
+
+func convertDatesMapRecursiveStep(jsonObj map[string]interface{}) (map[string]interface{}, error) {
+	res := make(map[string]interface{})
+	for k, v := range jsonObj {
+		switch vv := v.(type) {
+		case []interface{}:
+			arr, err := convertDatesSliceRecursiveStep(vv)
+			if err != nil {
+				return nil, err
+			}
+			res[k] = arr
+		case map[string]interface{}:
+			dic, err := convertDatesMapRecursiveStep(vv)
+			if err != nil {
+				return nil, err
+			}
+			res[k] = dic
 		default:
 			if k == "iat" || k == "exp" || k == "nbf" {
 				if epoch, ok := v.(float64); ok {
 					// update map with time from epoch to human format
-					m[k] = time.Unix(int64(epoch), 0).UTC().String()
+					res[k] = time.Unix(int64(epoch), 0).UTC().String()
+					continue
 				}
 			}
+			res[k] = vv
 		}
-	}
-	res, err := json.Marshal(m)
-	if err != nil {
-		return nil, errors.New("failed to re-convert claims to json")
 	}
 	return res, nil
 }
